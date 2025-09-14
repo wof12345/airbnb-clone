e2bf7950af4551f13502816b8ab3f5a5caf5189a
wof12345 <lsampercot@gmail.com>
2025-09-15 01:47:03 +0600

    test :  dep

--- backend/app.ts
+++ backend/app.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/services", serviceRoutes);
+app.use("/services", serviceRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server Running...");
